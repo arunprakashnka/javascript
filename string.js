edca6f878f2f44c59c3489d7f86517785725ad6c
arunprakashnka <arunprakashnka@gmail.com>
2023-02-21 09:32:40 +0530

    string js file added

---
+++ string.js
@@ -0,0 +1,17 @@
+const string = "ASPIRE SYSTEMS SIPCOT";
+console.log(string);
+const badString = string;
+console.log(badString);
+console.log("single quote vs double quote:");
+const sgl = 'Single quotes.';
+const dbl = "Double quotes";
+console.log(sgl);
+console.log(dbl);
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+console.log(sglDbl);
+console.log(dblSgl);
+console.log("concatenating string:");
+const name = "arun prakash";
+const greeting = `Hello, ${name}`;
+console.log(greeting); // "Hello, Chris"
